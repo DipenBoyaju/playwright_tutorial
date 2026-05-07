0f344a8cedd758e012ed5fc815be62d579dd3fa3
Dipen <dipendraboyaju222@gmail.com>
2026-05-07 17:16:55 +0545

    group

---
+++ tests/grouping.spec.js
@@ -0,0 +1,40 @@
+import { test } from "@playwright/test";
+
+
+test.beforeAll(async () => {
+  console.log('this is beforeAll hook....')
+})
+
+test.afterAll(async () => {
+  console.log('this is afterAll hook....')
+})
+
+test.beforeEach(async () => {
+  console.log('this is beforeEach hook....')
+})
+
+test.afterEach(async () => {
+  console.log('this is afterEach hook....')
+})
+
+test.describe('Group 1', () => {
+  test('test1', async ({ page }) => {
+    console.log('this is test 1 ....')
+  });
+
+  test('test2', async ({ page }) => {
+    console.log('this is test 2 ....')
+  });
+})
+
+
+test.describe('Group 2', () => {
+  test('test3', async ({ page }) => {
+    console.log('this is test 3 ....')
+  });
+
+  test('test4', async ({ page }) => {
+    console.log('this is test 4 ....')
+
+  });
+})
