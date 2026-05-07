49fc6fa05ad1f486909d3f93e0e60257ba5b698b
Dipen <dipendraboyaju222@gmail.com>
2026-05-07 13:24:59 +0545

    hooks

---
+++ tests/hooks1.spec.js
@@ -0,0 +1,49 @@
+import { expect, test } from "@playwright/test";
+
+test("Home Page Test", async ({ page }) => {
+  page.goto("https://demoblaze.com/index.html");
+
+  //login
+  await page.locator("//a[@id='login2']").click();
+  await page.locator("//input[@id='loginusername']").fill("pavanol");
+  await page.locator("//input[@id='loginpassword']").fill("test@123");
+  await page.locator("button[onclick='logIn()']").click();
+
+  //home page
+  const product = await page.$$(".hrefch");
+  await expect(product).toHaveLength(9);
+
+  //logout
+  await page.locator('#logout2').click();
+
+  await page.waitForTimeout(3000);
+
+})
+
+test.only("Add Product to cart Test", async ({ page }) => {
+  page.goto("https://demoblaze.com/index.html");
+
+  //login
+  await page.locator("//a[@id='login2']").click();
+  await page.locator("//input[@id='loginusername']").fill("pavanol");
+  await page.locator("//input[@id='loginpassword']").fill("test@123");
+  await page.locator("button[onclick='logIn()']").click();
+
+  //add to cart
+  await page.locator("//a[normalize-space()='Samsung galaxy s6']").click();
+
+  (await page.waitForSelector("//a[normalize-space()='Add to cart']"));
+  await page.locator("//a[normalize-space()='Add to cart']").click();
+
+  page.on('dialog', async dialog => {
+    expect(dialog.message()).toContain('Product added.');
+    await dialog.accept();
+  })
+
+  await page.waitForTimeout(3000)
+
+  //logout
+  await page.locator('#logout2').click();
+
+  await page.waitForTimeout(3000);
+})
---
+++ tests/hooks3.spec.js
@@ -0,0 +1,66 @@
+import { expect, test } from "@playwright/test";
+
+let page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+  page.goto("https://demoblaze.com/index.html");
+
+  //login
+  await page.locator("//a[@id='login2']").click();
+  await page.locator("//input[@id='loginusername']").fill("pavanol");
+  await page.locator("//input[@id='loginpassword']").fill("test@123");
+  await page.locator("button[onclick='logIn()']").click();
+})
+
+test.afterAll(async () => {
+  await page.locator('#logout2').click();
+})
+
+test("Home Page Test", async () => {
+  // page.goto("https://demoblaze.com/index.html");
+
+  //login
+  // await page.locator("//a[@id='login2']").click();
+  // await page.locator("//input[@id='loginusername']").fill("pavanol");
+  // await page.locator("//input[@id='loginpassword']").fill("test@123");
+  // await page.locator("button[onclick='logIn()']").click();
+
+  //home page
+  const product = await page.$$(".hrefch");
+  await expect(product).toHaveLength(9);
+
+  //logout
+  // await page.locator('#logout2').click();
+
+  await page.waitForTimeout(3000);
+
+})
+
+test("Add Product to cart Test", async () => {
+  // page.goto("https://demoblaze.com/index.html");
+
+  //login
+  // await page.locator("//a[@id='login2']").click();
+  // await page.locator("//input[@id='loginusername']").fill("pavanol");
+  // await page.locator("//input[@id='loginpassword']").fill("test@123");
+  // await page.locator("button[onclick='logIn()']").click();
+
+  //add to cart
+  await page.locator("//a[normalize-space()='Samsung galaxy s6']").click();
+
+  (await page.waitForSelector("//a[normalize-space()='Add to cart']"));
+  await page.locator("//a[normalize-space()='Add to cart']").click();
+
+  page.on('dialog', async dialog => {
+    expect(dialog.message()).toContain('Product added.');
+    await dialog.accept();
+  })
+
+  await page.waitForTimeout(3000)
+
+  //logout
+  // await page.locator('#logout2').click();
+
+  await page.waitForTimeout(3000);
+})
